58dbae3a8bdcdaefc9c5bdf0fadd00f91ff6f685
raxeraditya <143254117+raxeraditya@users.noreply.github.com>
2023-12-17 16:43:23 +0530

    Update init_method.py

--- init_method.py
+++ init_method.py
@@ -1,7 +1,7 @@
 # __init__() method
-# The __init__ method is similar to constructors in C++ and Java.
-# Constructors are used to initializing the object’s state.
-# Like methods, a constructor also contains a collection of statements(i.e. instructions) that are executed at the time of Object creation. It runs as soon as an object of a class is instantiated. The method is useful to do any initialization you want to do with your object.
+# constructor is allways executed first. while call a class 
+# The __init__ method is similar to constructors in C++ and Java script.
+# create a variable within the class or method then allways use self just like this in js
 
 
 # Sample class with init method
@@ -9,15 +9,11 @@ class Person:
 
     # init method or constructor
     def __init__(self, name):
-        self.name = name
+        self.sname = name
 #   self.name is a variable to store class argument 
     # Sample Method
     def say_hi(self):
-        print('Hello, my name is', self.name)
-
-#   person ek class ka nam he chuki isme constructor ka use hua he isliye class me hame vo argument pass 
-#   karna pad raha he jo self , name he name is a passing argument through a class
-#   because init is executed without calling method(function)
+        print('Hello, my name is', self.sname)
 p = Person('Nikhil')
 p.say_hi()
 
@@ -31,4 +27,4 @@ class tor:
         print("hey run")
 
 name = tor()
-name.mn(5,6)
+name.mn(6,6)
